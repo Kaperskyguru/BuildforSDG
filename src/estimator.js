ff32d56929665d2f5bfe3a9560c08e77877bc1ff
kaperskyguru <solomoneseme@gmail.com>
2020-04-10 22:13:27 +0100

    Challenge 2 still on bugs

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,33 @@
 const covid19ImpactEstimator = (data) => {
-  const input = data;
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currectlyInfected = data.reportedCases * 10;
+
+  impact.infectionsByRequestedTime = impact.currectlyInfected * 1024;
+
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+
+  impact.hospitalBedsByRequestedTime =
+    Math.floor(data.totalHospitalBeds * 0.35) -
+    impact.severeCasesByRequestedTime;
+
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currectlyInfected * 1024;
+
+  severeImpact.severeCasesByRequestedTime =
+    severeImpact.infectionsByRequestedTime * 0.15;
+
+  severeImpact.hospitalBedsByRequestedTime =
+    Math.floor(data.totalHospitalBeds * 0.35) -
+    severeImpact.severeCasesByRequestedTime;
+
   return {
-    data: input,
-    impact: {
-      currectlyInfected: data.reportedCases * 10,
-      infectionsByRequestedTime: impact.currectlyInfected * 1024,
-      severeCasesByRequestedTime: impact.infectionsByRequestedTime * 0.15,
-      hospitalBedsByRequestedTime:
-        Math.floor(data.totalHospitalBeds * 0.35) -
-        impact.severeCasesByRequestedTime
-    },
-    severeImpact: {
-      currentlyInfected: data.reportedCases * 50,
-      infectionsByRequestedTime: severeImpact.currectlyInfected * 1024,
-      severeCasesByRequestedTime: severeImpact.infectionsByRequestedTime * 0.15,
-      hospitalBedsByRequestedTime:
-        Math.floor(data.totalHospitalBeds * 0.35) -
-        severeImpact.severeCasesByRequestedTime
-    }
+    data,
+    impact,
+    severeImpact
   };
 };
 
