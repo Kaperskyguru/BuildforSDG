442e34a8b1b2646b2fac7c465364775f73bbfbf8
kaperskyguru <solomoneseme@gmail.com>
2020-04-10 21:26:22 +0100

    Challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  return {
+    data,
+    impact: {
+      currectlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: impact.currectlyInfected * 1024,
+      severeCasesByRequestedTime: impact.infectionsByRequestedTime * 0.15,
+      hospitalBedsByRequestedTime:
+        Math.floor(data.totalHospitalBeds * 0.35) -
+        impact.severeCasesByRequestedTime
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: severeImpact.currectlyInfected * 1024,
+      severeCasesByRequestedTime: severeImpact.infectionsByRequestedTime * 0.15,
+      hospitalBedsByRequestedTime:
+        Math.floor(data.totalHospitalBeds * 0.35) -
+        severeImpact.severeCasesByRequestedTime
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
